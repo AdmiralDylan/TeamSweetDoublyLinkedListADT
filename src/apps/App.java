b0d131f0740467f6a83df907be536deb3d52b425
Andrew Contreras <127986784+ndrewontreras@users.noreply.github.com>
2023-11-08 22:38:52 -0500

    Added additional test cases

--- src/apps/App.java
+++ src/apps/App.java
@@ -9,88 +9,214 @@ import nodes.DLLNode;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        final int TESTING_TIME_IN_ms = 100;
 
-        final int TESTING_TIME_IN_ms = 200;
-        
-        String[] numList = new String[]{"a","z","c","z","d","e","k","g","h","i","j"};
-        
-        DLLList<String> list = new DLLList<String>();
-        
+        DLLList<String> listString = new DLLList<>();
+        DLLList<Boolean> listBool = new DLLList<>();
+        DLLList<Integer> listInt = new DLLList<>();
 
-        for(String a:numList){
-            list.add(a);
-            System.out.println(list.size());
-            System.out.println(list.toString());
+        String[] stringArray = new String[]{"a","z","c","z","d","e","k","g","h","i","j"};
+        boolean[] boolArray = new boolean[]{false, true, false, true, true, false, false};
+        int[] intArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("------------------");
+        System.out.println("String List Test\n");
+
+        for(String a:stringArray){
+            listString.add(a);
+            System.out.println("Size: " + listString.size());
+            System.out.println("List: " + listString);
             Thread.sleep(TESTING_TIME_IN_ms);
         }
 
-        System.out.println(list.toString());
+        System.out.println("Removed 'a'? " + listString.remove("a"));
+        System.out.println("List after removing 'a': " + listString);
+        System.out.println("List size after removing 'a': " + listString.size());
 
-        list.remove("a");
-        System.out.println(list.toString());
-        System.out.println(list.size());
-        
-        System.out.println(list.contains("a"));
+        System.out.println("Contains 'a'? " + listString.contains("a"));
 
         Thread.sleep(TESTING_TIME_IN_ms);
 
-        list.add("b");
-        System.out.println(list.toString());
-        System.out.println(list.size());
+        listString.add("b");
+        System.out.println("List after adding 'b': " + listString);
+        System.out.println("List size after adding 'b': " + listString.size());
 
         Thread.sleep(TESTING_TIME_IN_ms);
 
-        list.add("a");
-        System.out.println(list.toString());
-        System.out.println(list.size());
+        listString.add("a");
+        System.out.println("List after adding 'a': " + listString);
+        System.out.println("List size after adding 'a': " + listString.size());
 
         Thread.sleep(TESTING_TIME_IN_ms);
 
-        System.out.println(list.get("a"));
-        System.out.println(list.get("q"));
-        System.out.println(list.contains("a"));
-        System.out.println(list.contains("q"));
+        System.out.print("Get 'a': ");
+        System.out.println(listString.get("a"));
+
+        System.out.println(listString.get("q"));
+
+        System.out.println(listString.contains("a"));
+        System.out.println(listString.contains("q"));
 
         Thread.sleep(TESTING_TIME_IN_ms);
-
 
         int i = 0;
-
-        System.out.println("Enhanced for" + "\n");
-        for(String a:list){
-            System.out.println(a + " index at " + i);
+        System.out.println("Enhanced for-loop:");
+        for(String a:listString){
+            System.out.print(a + " Index:" + i + ", ");
             i++;
             Thread.sleep(TESTING_TIME_IN_ms);
         }
 
-        System.out.println(list.size());
+        Iterator<String> stringLinItr = listString.iterator();
+        Iterator<String> stringRandItr = listString.iteratorRandom();
 
-        System.out.println("linear iter");
-        Iterator<String> itrLine = list.iterator(); 
-        Iterator<String> itrRand1 = list.iteratorRandom(); 
-        Iterator<String> itrRand2 = list.iteratorRandom(); 
-
-        while(itrLine.hasNext()){
-            System.out.print(itrLine.next() + ",");
+        System.out.println("\nLinear iteration: ");
+        while(stringLinItr.hasNext()){
+            System.out.print(stringLinItr.next() + ",");
             Thread.sleep(TESTING_TIME_IN_ms);
         }
 
-        System.out.println("\nrandom iter1");
-        while(itrRand1.hasNext()){
-            System.out.print(itrRand1.next() + ",");
+        System.out.println("\nRandom iteration: ");
+        while(stringRandItr.hasNext()){
+            System.out.print(stringRandItr.next() + ",");
             Thread.sleep(TESTING_TIME_IN_ms);
         }
 
-        System.out.println("\nrandom iter2");
-        while(itrRand2.hasNext()){
-            System.out.print(itrRand2.next() + ",");
+        System.out.println("\n\n------------------");
+        System.out.println("Boolean List Test\n");
+
+        for(boolean a:boolArray){
+            listBool.add(a);
+            System.out.println("Size: " + listBool.size());
+            System.out.println("List: " + listBool);
             Thread.sleep(TESTING_TIME_IN_ms);
         }
-    
-        try{
-            System.out.print(itrLine.next());
-        } catch (Exception e){
-            System.out.println("\n" + e);
+
+        System.out.println("Removed first 'true' value?: " + listBool.remove(true));
+        System.out.println("List after removing first 'true' value: " + listBool);
+        System.out.println("List size after removing first 'true' value: " + listBool.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        for(boolean a:listBool) {
+            if(a == false) {
+                listBool.remove(a);
+            }
+        }
+        System.out.println("List after removing all 'false' values: " + listBool);
+        //System.out.println("Contains 'false'?: " + listBool.contains(false));
+        System.out.println("List size after removing all false values: " + listBool.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        listBool.add(false);
+        System.out.println("List after adding a 'false' value: " + listBool);
+        System.out.println("List size after adding a 'false' value: " + listBool.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        listBool.add(true);
+        System.out.println("List after adding a 'true' value: " + listBool);
+        System.out.println("List size after adding a 'true' value: " + listBool.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        listBool.add(false);
+        System.out.println("List after adding a 'false' value: " + listBool);
+        System.out.println("List size after adding a 'false' value: " + listBool.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        System.out.print("Get 'false': ");
+        System.out.println(listBool.get(false));
+
+        System.out.println("Enhanced for-loop:");
+        int j = 0;
+        for(boolean a:listBool){
+            System.out.print(a + " Index:" + j + ", ");
+            j++;
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        Iterator<Boolean> boolLinItr = listBool.iterator();
+        Iterator<Boolean> boolRandItr = listBool.iteratorRandom();
+
+        System.out.println("\nLinear iteration: ");
+        while(boolLinItr.hasNext()){
+            System.out.print(boolLinItr.next() + ",");
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        System.out.println("\nRandom iteration");
+        while(boolRandItr.hasNext()){
+            System.out.print(boolRandItr.next() + ",");
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        System.out.println("\n\n------------------");
+        System.out.println("Integer List Test\n");
+
+        for(int a:intArray){
+            listInt.add(a);
+            System.out.println("Size: " + listInt.size());
+            System.out.println("List: " + listInt);
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        System.out.println("Removed '3'?: " + listInt.remove(3));
+        System.out.println("List after removing '3': " + listInt);
+        System.out.println("List size after removing '3': " + listInt.size());
+        System.out.println("Contains '3'? " + listInt.contains(3));
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        listInt.add(11);
+        System.out.println("List after adding '11': " + listInt);
+        System.out.println("List size after adding '11': " + listInt.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        listInt.add(3);
+        System.out.println("List after adding '3': " + listInt);
+        System.out.println("List size after adding '3': " + listInt.size());
+
+        Thread.sleep(TESTING_TIME_IN_ms);
+
+        /*
+        System.out.print("Get '4': ");
+        System.out.println(listInt.get(4));
+        */
+        System.out.print("Get '18': ");
+        System.out.println(listInt.get(18));
+
+
+        System.out.println("Contains '6'? " + listInt.contains(6));
+
+        System.out.println("Contains '12'? " + listInt.contains(12));
+
+        System.out.println("Enhanced for-loop:");
+        int k = 0;
+        for(int a:listInt){
+            System.out.print(a + " Index:" + k + ", ");
+            k++;
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        Iterator<Integer> intLinItr = listInt.iterator();
+        Iterator<Integer> intRandItr = listInt.iteratorRandom();
+
+        System.out.println("\nLinear iteration: ");
+        while(intLinItr.hasNext()){
+            System.out.print(intLinItr.next() + ",");
+            Thread.sleep(TESTING_TIME_IN_ms);
+        }
+
+        System.out.println("\nRandom iteration");
+        while(intRandItr.hasNext()){
+            System.out.print(intRandItr.next() + ",");
+            Thread.sleep(TESTING_TIME_IN_ms);
         }
     }
+       
 }
